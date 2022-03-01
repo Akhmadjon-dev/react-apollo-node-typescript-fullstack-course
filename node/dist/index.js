43706431dc503762e47afe0a0ddcd7dc97141aee
akhmadjon-dev <info.ahmadjon@gmail.com>
2022-03-01 09:48:39 +0500

    postgresql initialized

--- node/dist/index.js
+++ node/dist/index.js
@@ -1,2 +1,20 @@
-console.log('lets go hel yeah');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const main = async () => {
+    const orm = await core_1.MikroORM.init({
+        entities: [__dirname + "/entities/*.ts"],
+        dbName: "mikro_orm",
+        type: "postgresql",
+        host: "localhost",
+        port: 5432,
+        user: "postgres",
+        password: "postgres",
+    });
+    await orm.getSchemaGenerator().dropSchema();
+    await orm.getSchemaGenerator().createSchema();
+    await orm.close();
+};
+main().catch(err => console.error(err));
+console.log("lets go hel yeah");
 //# sourceMappingURL=index.js.map
--- node/src/index.ts
+++ node/src/index.ts
@@ -1,1 +1,20 @@
-console.log('lets go hel yeah');
+import { MikroORM } from "@mikro-orm/core";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [__dirname + "/entities/*.ts"],
+        dbName: "mikro_orm",
+        type: "postgresql",
+        host: "localhost", 
+        port: 5432,
+        user: "postgres",
+        password: "postgres",
+    });
+    await orm.getSchemaGenerator().dropSchema();
+    await orm.getSchemaGenerator().createSchema();
+    await orm.close();
+};
+
+main().catch(err => console.error(err));
+
+console.log("lets go hel yeah");
